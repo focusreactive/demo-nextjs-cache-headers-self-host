b7b97f40c6c3c0fd617638cbf3b6e6c30f23140f
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2023-11-29 02:02:28 +0000

    configure cachign for static files in public folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [];
+    }
+
+    return [
+      {
+        source: '/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000',
+          }
+        ],
+      }
+    ];
+  },
+}
 
 module.exports = nextConfig
