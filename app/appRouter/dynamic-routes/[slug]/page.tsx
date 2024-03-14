a363d0e2bd40d46f66e10677077b185c9404defc
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2024-03-14 17:14:46 +0000

    add dynamic config for dynamic app router page

--- app/appRouter/dynamic-routes/[slug]/page.tsx
+++ app/appRouter/dynamic-routes/[slug]/page.tsx
@@ -1,5 +1,5 @@
 export default function Page({ params }: { params: { slug: string } }) {
   return <div>My Post: {params.slug}</div>
 }
-
+export const dynamic = 'error'
 export const revalidate = 600
