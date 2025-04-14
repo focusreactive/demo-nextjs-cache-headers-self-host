04e972d2602571b0afcd280103feea613e31f3f5
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2025-04-14 21:03:03 +0100

    return examples for pages router

--- next.config.js
+++ next.config.js
@@ -56,6 +56,53 @@ const nextConfig = {
           },
         ],
       },
+
+      // **
+      // BELOW EXAMPLES FOR PAGES ROUTER
+      // **
+
+      {
+        // this page has both html and JS representation, we're only interested in HTML, as JS is hashed
+        source: '/fullyStaticPage',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=60, s-maxage=600, stale-while-revalidate=14400, stale-if-error=14400',
+          }
+        ],
+      },
+      {
+        // this page would have both html and json representation of page data that both need to be cached with same policy for consistency
+        source: '/(.*)(getStaticProps|getStaticProps\.json)',
+        headers: [
+          {
+            // Example of Cloudflare custom header that takes priority over Cache-Control. Fastly for example hasSurrogate-Control
+            key: 'CDN-Cache-Control',
+            value: 'max-age=600, stale-while-revalidate=14400, stale-if-error=14400',
+          },
+        ],
+      },
+      {
+        // we need to cover all json data for SPA navigations under the path that may have multiple params as well as the HTML page in section below
+        source: '/(.*)/getStaticPaths/(.*)(json)',
+        headers: [
+          {
+            // Example of Cloudflare custom header that takes priority over Cache-Control. Fastly for example hasSurrogate-Control
+            key: 'CDN-Cache-Control',
+            value: 'max-age=600, stale-while-revalidate=14400, stale-if-error=14400',
+          },
+        ],
+      },
+      {
+        source: '/getStaticPaths/:name',
+        headers: [
+          {
+            // Example of Cloudflare custom header that takes priority over Cache-Control. Fastly for example hasSurrogate-Control
+            key: 'CDN-Cache-Control',
+            value: 'max-age=600, stale-while-revalidate=14400, stale-if-error=14400',
+          },
+        ],
+      },
     ];
   },
 }
